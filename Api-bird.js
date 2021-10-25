cace05909cf441e87e7addbae66c44007236b417
Weslley30 <76874062+Weslley30@users.noreply.github.com>
2021-10-25 14:11:51 -0300

    Add files via upload

--- Api-bird.js
+++ Api-bird.js
@@ -1,18 +1,113 @@
-function showTime (){
-  var time = new Date();
-  var hour = time.getHours();
-  var minute = time.getMinutes();
-  var second = time.getSeconds();
+image_array_aguia = [
+    src = "https://www.infoescola.com/wp-content/uploads/2008/07/%C3%A1guia_263456444.jpg",
+    src = "https://s2.glbimg.com/ufjghiiGBs-96sOtH0Naj6cu0zM=/smart/e.glbimg.com/og/ed/f/original/2020/08/14/gettyimages-1174172388_2.jpg",
+    src = "https://www.passaro.org/wp-content/uploads/2018/10/aninhamento-da-aguia-careca.jpg",
+    src = "https://www.greenme.com.br/wp-content/uploads/2020/11/aguia.jpg",
+    src = "https://a-static.mlcdn.com.br/618x463/quadro-decorativo-aguia-ave-animais-decoracao-com-moldura-vital-quadros/vitalquadrosdobrasil/33321454/ab7de8201912f4d31959e56f5183d204.jpg",
+    src = "https://i1.wp.com/www.horademudar.com.br/wp-content/uploads/2012/07/aguia.jpg?resize=351%2C300",
+    src = "https://s3.amazonaws.com/media.wikiaves.com.br/images/7542/2457838_e95aeb66f5383b33de1069c88c9e99db.jpg",
+    src = "https://44477.cdn.simplo7.net/static/44477/sku/bandeiras-de-adoracao-bandeira-bda211-aguia-bandeira-brasil-profecia--p-1593129876945.png"
+]
 
-  if(hour<10) hour = "0"+hour;
-  if(minute<10) minute = "0"+minute;
-  if(second<10) second = "0"+second;
+function get_random_aguia(){
 
-  var tempo = hour+":"+minute+":"+second;
-  document.getElementById("timer").innerHTML=tempo
+    random_index = Math.floor(Math.random() * image_array_aguia.length);
+    selected_image = image_array_aguia[random_index]
+    document.getElementById('image_shower').src = selected_image;
+    document.getElementById('timer').innerHTML=Date();
 }
 
-function initTime(){
-  setInterval (showTime, 1000);
+image_array_arara = [
+  src = "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/arara-azul.jpg ",
+  src = "https://i0.wp.com/www.ondapocos.com.br/wp-content/uploads/2020/10/Arara-azul-chama-a-aten%C3%A7%C3%A3o-pela-sua-beleza.jpg?fit=1772%2C1181&ssl=1 ",
+  src = "https://www.passaro.org/wp-content/uploads/2018/10/Arara-Azul-Reprodu%C3%A7%C3%A3o-750x405.jpg ",
+  src = "https://www.infoescola.com/wp-content/uploads/2016/08/arara-azul-635338985.jpg ",
+  src = "https://s3.amazonaws.com/media.wikiaves.com.br/images/2062/2602166_0143f5cc22b6e5f239aaa43766a7303e.jpg ",
+  src = "https://blog.pescagerais.com.br/wp-content/uploads/2021/04/arara-azul-anodorhynchus-hyacinthinus.jpg ",
+  src = "https://tribunadejundiai.com.br/wp-content/uploads/2020/03/ararinha-00-min.jpg ",
+  src = "https://ciclovivo.com.br/wp-content/uploads/2015/009/img/noticias/arara-azul-nat-geo.jpg "
+]
+
+function get_random_arara(){
+
+  random_index = Math.floor(Math.random() * image_array_arara.length);
+  selected_image = image_array_arara[random_index]
+  document.getElementById('image_shower').src = selected_image;
+  document.getElementById('timer').innerHTML=Date();
 }
 
+image_array_picaPau = [
+  src = "https://upload.wikimedia.org/wikipedia/commons/3/36/Woodpecker_20040529_151837_1c.jpeg ",
+  src = "https://s3.amazonaws.com/media.wikiaves.com.br/images/6242/2426342_ce45cdb342678f8d117327b8e5dcca64.jpg ",
+  src = "https://t.ctcdn.com.br/G4AF53ftKXkGONcb9jAVVMe7voc=/0x0:769x433/769x433/smart/i511100.png ",
+  src = "https://i.ytimg.com/vi/1q40Vh8rP5U/maxresdefault.jpg ",
+  src = "https://1.bp.blogspot.com/-13J43tllbq4/Wr5Pm5v1n5I/AAAAAAAAN1U/36gPD3E-UbgSHZWybA-FTU5wQzdAVpBtQCLcBGAs/s1600/IMG_3449.jpg ",
+  src = "https://super.abril.com.br/wp-content/uploads/2021/09/pica-pau_facebook.jpg?quality=90&strip=info ",
+  src = "https://cdn.pixabay.com/photo/2012/06/17/01/33/woodpecker-50135_960_720.jpg ",
+  src = "https://cdn.pixabay.com/photo/2020/05/16/11/52/woodpecker-5177241_960_720.jpg "
+]
+
+function get_random_picaPau(){
+
+  random_index = Math.floor(Math.random() * image_array_picaPau.length);
+  selected_image = image_array_picaPau[random_index]
+  document.getElementById('image_shower').src = selected_image;
+  document.getElementById('timer').innerHTML=Date();
+}
+
+image_array_archaeopteryx = [
+  src = "https://static.turbosquid.com/Preview/2020/10/14__06_16_00/archaeopteryx_poses0022.jpg27B1FABA-9086-4C4E-8B68-07A2062293CELarge.jpg ",
+  src = "https://media.istockphoto.com/photos/archaeopteryx-picture-id600177006 ",
+  src = "https://veja.abril.com.br/wp-content/uploads/2016/05/xiaotingia-zhengi-original1.jpeg ",
+  src = "https://http2.mlstatic.com/D_NQ_NP_606537-CBT44602197734_012021-O.jpg ",
+  src = "https://static.wikia.nocookie.net/dinossauros-fanfiction/images/e/ef/Archeo.jpg/revision/latest?cb=20190216192611&path-prefix=pt-br ",
+  src = "https://www.achetudoeregiao.com.br/dinossauros/dinossauros.gif/archaeopteryx.jpg ",
+  src = "https://netnature.files.wordpress.com/2018/03/sem-tc3adtulo.png?w=551 ",
+  src = "https://cdna.artstation.com/p/assets/images/images/003/463/374/large/vlad-konstantinov-archaeopteryx-01-3-1600.jpg?1473949286 "
+]
+
+function get_random_archaeopteryx(){
+
+  random_index = Math.floor(Math.random() * image_array_archaeopteryx.length);
+  selected_image = image_array_archaeopteryx[random_index]
+  document.getElementById('image_shower').src = selected_image;
+  document.getElementById('timer').innerHTML=Date();
+}
+
+image_array_pelagornis = [
+  src = "http://s2.glbimg.com/3gnP6Vz8c-xvXwaBMn_CXzBYQHo=/290x217/s2.glbimg.com/-6Fh53zKg5RmcW45s7svygxo9eI=/s.glbimg.com/jo/g1/f/original/2014/07/07/wingspan.jpg ",
+  src = "http://2.bp.blogspot.com/_1nuzdTcJ1wQ/TJk3fpokSGI/AAAAAAAAMY0/vG4cU8z69SI/s1600/ave+grande.jpg ",
+  src = "http://pm1.narvii.com/7056/827a47f0c2d29b90bbe853b01f731b581b5b4fe2r1-400-203v2_00.jpg ",
+  src = "http://pm1.narvii.com/7032/addd8aab6106abb9996599ec1d0617bfe3c3f960r1-1280-720v2_uhq.jpg ",
+  src = "https://upload.wikimedia.org/wikipedia/commons/d/d2/Pelagornis_chilensis_EF_blue.jpg ",
+  src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyQmvCvA-l6ptZJDfzI5dWrybQW6s_kPeBAw&usqp=CAU ",
+  src = "https://e7.pngegg.com/pngimages/841/325/png-clipart-ark-survival-evolved-pelagornis-sandersi-dinosaur-stork-ark-shell-miscellaneous-fictional-character.png ",
+  src = "https://i.redd.it/0swtr1esuly51.png "
+]
+
+function get_random_pelagornis(){
+
+  random_index = Math.floor(Math.random() * image_array_pelagornis.length);
+  selected_image = image_array_pelagornis[random_index]
+  document.getElementById('image_shower').src = selected_image;
+  document.getElementById('timer').innerHTML=Date();
+}
+
+image_array_pterodáctilo = [
+  src = "https://www.infoescola.com/wp-content/uploads/2010/07/pterod%C3%A1ctilo_188956646.jpg ",
+  src = "https://aventurasnahistoria.uol.com.br/media/_versions/arqueologia/ictiossauro_fArvDOl_widelg.jpg ",
+  src = "https://media.istockphoto.com/photos/pterodactylus-picture-id615096328 ",
+  src = "https://images-americanas.b2w.io/produtos/2107768669/imagens/modelo-de-figura-de-acao-de-dinossauro-pterodactilo-realista/2107768677_1_large.jpg ",
+  src = "https://www.portaldosanimais.com.br/wp-content/uploads/2020/06/Pterossauro-1-640x330.jpg ",
+  src = "https://static.wikia.nocookie.net/dinossauros-fanfiction/images/9/9b/Dactilo.jpg/revision/latest/top-crop/width/360/height/450?cb=20190225211220&path-prefix=pt-br ",
+  src = "http://pm1.narvii.com/6596/280960af0020ddd010789c6e386b49a2287978b9_00.jpg ",
+  src = "http://revistagalileu.globo.com/Revista/Galileu2/foto/0,,41471905,00.jpg "
+]
+
+function get_random_pterodáctilo(){
+
+  random_index = Math.floor(Math.random() * image_array_pterodáctilo.length);
+  selected_image = image_array_pterodáctilo[random_index]
+  document.getElementById('image_shower').src = selected_image;
+  document.getElementById('timer').innerHTML=Date();
+}
